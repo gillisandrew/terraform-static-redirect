7e1e33263d9baef55affc04430f1ed714d5b54aa
Andrew Gillis <gillis.andrew@gmail.com>
2020-02-07 17:03:55 -0500

    initial commit

---
+++ inputs.tf
@@ -0,0 +1,21 @@
+variable "project" {
+  type    = string
+  default = "example"
+}
+
+variable "environment" {
+  type    = string
+  default = "dev"
+}
+
+variable "domain" {
+  type = string
+}
+
+variable "target" {
+  type = string
+}
+
+variable "hosted_zone_id" {
+  type = string
+}
